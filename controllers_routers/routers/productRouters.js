92d71e7d7eb234b8858558183823bd0829078d69
keyzafirstanto <rfirstanto@gmail.com>
2021-10-08 22:20:44 +0700

    BE-03 admin is able to delete product

--- controllers_routers/routers/productRouters.js
+++ controllers_routers/routers/productRouters.js
@@ -5,5 +5,6 @@ const routers = require('express').Router();
 routers.get('/get', productControllers.getAllProductData);
 routers.post('/post', productControllers.addData);
 routers.patch('/update/:product_id', productControllers.updateData);
+routers.delete('/delete/:product_id', productControllers.deleteData);
 
 module.exports = routers;
