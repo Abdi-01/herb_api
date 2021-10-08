7970382f783f0464acee81f4596b290761f6df05
keyzafirstanto <rfirstanto@gmail.com>
2021-10-08 22:16:04 +0700

    BE-01 (updated) admin input new data

--- controllers_routers/routers/productRouters.js
+++ controllers_routers/routers/productRouters.js
@@ -4,7 +4,5 @@ const routers = require('express').Router();
 
 routers.get('/get', productControllers.getAllProductData);
 routers.post('/post', productControllers.addData);
-routers.patch('/update/:product_id', productControllers.updateData);
-routers.delete('/delete/:product_id', productControllers.deleteData);
 
 module.exports = routers;
