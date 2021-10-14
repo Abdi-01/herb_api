043367142af7a76a6ce6ef669bdec208faca7325
keyzafirstanto <rfirstanto@gmail.com>
2021-10-14 20:02:25 +0700

    BE-04 user is able to get data from api and show them on front end

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const authController = require('./authController');
 const productControllers = require('./productControllers');
-const transactionControllers = require('./transactionControllers');
 
 module.exports = {
   authController,
   productControllers,
-  transactionControllers,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const authRouters = require('./authRouters');
 const productRouters = require('./productRouters');
-const transactionRouters = require('./transactionRouters');
 
 module.exports = {
   authRouters,
   productRouters,
-  transactionRouters,
 };
