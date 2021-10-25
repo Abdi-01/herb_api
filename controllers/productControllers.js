d806e3ab8e906b3b8386b947a45727879eb20504
keyzafirstanto <rfirstanto@gmail.com>
2021-10-25 19:53:02 +0700

    HERB-40-KEYZA(updated) nambahin foto products

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,7 +1,7 @@
-const { db } = require("../database/index");
+const { db } = require('../database/index');
 
-const { uploader } = require("../helper/uploader");
-const fs = require("fs");
+const { uploader } = require('../helper/uploader');
+const fs = require('fs');
 
 module.exports = {
   getData: (req, res) => {
@@ -30,8 +30,8 @@ module.exports = {
   },
   addData: (req, res) => {
     try {
-      let path = "/images/products";
-      const upload = uploader(path, "PRD").fields([{ name: "file" }]);
+      let path = '/images/products';
+      const upload = uploader(path, 'PRD').fields([{ name: 'file' }]);
 
       upload(req, res, (error) => {
         // if error
@@ -41,7 +41,7 @@ module.exports = {
         }
         // console.log(req.files);
         const { file } = req.files;
-        const filepath = file ? path + "/" + file[0].filename : null;
+        const filepath = file ? path + '/' + file[0].filename : null;
 
         //parsing the data
         let data = JSON.parse(req.body.data);
@@ -61,17 +61,17 @@ module.exports = {
           ${db.escape(price_per_unit)}, 
           ${db.escape(data.price_per_stock)}, 
           ${db.escape(data.brand_id)}, 
-          ${db.escape(data.category_id)});`;
+          ${db.escape(data.category_id)}, null);`;
 
         db.query(addNewDataQuery, (err, results) => {
           if (err) {
             console.log(err);
-            fs.unlinkSync("./public" + filepath);
+            fs.unlinkSync('./public' + filepath);
             res.status(500).send(err);
           }
           res
             .status(200)
-            .send({ message: "New Item has succefully been added" });
+            .send({ message: 'New Item has succefully been added' });
         });
       });
     } catch (error) {
@@ -81,8 +81,8 @@ module.exports = {
   },
   updateData: (req, res) => {
     try {
-      let path = "/images/products";
-      const upload = uploader(path, "PRD").fields([{ name: "file" }]);
+      let path = '/images/products';
+      const upload = uploader(path, 'PRD').fields([{ name: 'file' }]);
 
       upload(req, res, (error) => {
         // if error
@@ -93,7 +93,7 @@ module.exports = {
 
         if (req.files) {
           const { file } = req.files;
-          const filepath = file ? path + "/" + file[0].filename : null;
+          const filepath = file ? path + '/' + file[0].filename : null;
 
           let data = JSON.parse(req.body.data);
           data.product_img = filepath;
@@ -110,11 +110,11 @@ module.exports = {
             if (err) {
               console.log(err);
               res.status(500).send(err);
-              fs.unlinkSync("./public" + filepath);
+              fs.unlinkSync('./public' + filepath);
             }
             res
               .status(200)
-              .send({ message: "Item has succefully been updated" });
+              .send({ message: 'Item has succefully been updated' });
           });
         } else if (!req.files) {
           let data = req.body;
@@ -131,7 +131,7 @@ module.exports = {
               console.log(err);
               res.status(500).send(err);
             }
-            res.status(200).send({ message: "Succesfully updated item" });
+            res.status(200).send({ message: 'Succesfully updated item' });
           });
         }
       });
